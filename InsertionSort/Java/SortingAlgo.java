d14e4795c19410d84960a2c46d749588e380a06b
Shekhar Gupta <shekhargupta@Shekhar-Guptas-MacBook-Pro.local>
2012-06-21 23:07:48 +0530

    Generic Method implemented

--- InsertionSort/Java/SortingAlgo.java
+++ InsertionSort/Java/SortingAlgo.java
@@ -1,6 +1,5 @@
 package com.example.sort;
 
-import java.util.List;
 
 public class SortingAlgo<E> {
 
@@ -8,26 +7,33 @@ public class SortingAlgo<E> {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		int[] array = {5,2,4,3,6,1};
+		Integer[] array = {5,2,4,3,6,1};
+		String[] names = {"Shekhar","Ashish","Ashutosh"};
 		array = insertionSort(array);
+		names = insertionSort(names);
 		for(int elem : array){
 			System.out.println(elem);
+		}
+		for(String str : names){
+			System.out.println(str);
 		}
 
 	}
 	
 	
 	
-	public static <T> T[] insertionSort(T[] array){
+	public static <T extends Comparable> T[] insertionSort(T[] array){
 		int len = array.length;
 		for(int j=1;j < len;j++){
 			T key  = array[j];
 			int i = j-1;
-//			while(i > 0 && array[i] < array[j]){
-//				
-//			}
+			while(i >=0 && array[i].compareTo(key) > 0){
+				array[i+1] = array[i];
+				i -= 1;
+			}
+			array[i+1] = key;
 		}
-		return null;
+		return array;
 	}
 	
 	public static int[] insertionSort(int[] array){
